372a65d4dab6d94ca887170b61d6b88ca3c0cbf5
st120712 <zzw123@naver.com>
2025-05-07 10:48:53 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/aianalysisresultservice/AiAnalysisResultServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.aianalysisresultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAnalysisResultServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAnalysisResultServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/aianalysisresultservice/AiAnalysisResultServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.aianalysisresultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAnalysisResultServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
